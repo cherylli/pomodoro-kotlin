21443aa947bd89b26b043f58876471fa14cbf699
Cheryl Murphy <webmaster@cherylli.com>
2019-11-18 23:22:44 +1100

    Basic Interface
    Very basic starting interface

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_timer,menu)
+        return true
     }
 }
