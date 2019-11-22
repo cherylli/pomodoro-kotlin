1c7c64e4e9a3e1baa5af0626299838f3af67827d
Cheryl Murphy <webmaster@cherylli.com>
2019-11-22 23:46:59 +1100

    Add new interface with bottom nav
    change countdown timer UI from activity to fragment

---
+++ app/src/main/java/com/example/pomodorotimer/CountdownFragment.kt
@@ -0,0 +1,35 @@
+package com.example.pomodorotimer
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [CountdownFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [CountdownFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CountdownFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_countdown, container, false)
+    }
+
+}
