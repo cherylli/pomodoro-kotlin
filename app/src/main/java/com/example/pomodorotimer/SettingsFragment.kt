0dae53efb6e05bab6035ce2100cf58ccc56b80ba
Cheryl Murphy <webmaster@cherylli.com>
2019-11-25 20:40:35 +1100

    Add bottom nav and fragments
    Add countdown timer, timer setup and settings fragment and bottom nav bar

--- app/src/main/java/com/example/pomodorotimer/SettingsFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/SettingsFragment.kt
@@ -8,21 +8,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CountdownFragment : Fragment() {
-    companion object {
-        fun newInstance()=CountdownFragment()
-    }
-
-
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_countdown, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
-
-
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
 }
--- app/src/main/java/com/example/pomodorotimer/TimerSetupFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/TimerSetupFragment.kt
@@ -8,21 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CountdownFragment : Fragment() {
-    companion object {
-        fun newInstance()=CountdownFragment()
-    }
 
-
+class TimerSetupFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_countdown, container, false)
+        return inflater.inflate(R.layout.fragment_timer_setup, container, false)
     }
 
 
-
+    companion object {
+        fun newInstance() = TimerSetupFragment()
+    }
 }
