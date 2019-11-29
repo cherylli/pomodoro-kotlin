a121943ded6b4168d23673f4752c906d03701872
Cheryl Murphy <webmaster@cherylli.com>
2019-11-29 12:35:44 +1100

    Fix crashing on switch
    but still not actually switching fragments

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,11 +17,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //old toolbar and bottom nav
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //val bottomNavigation:BottomNavigationView = findViewById(R.id.bottom_navigation)
+        //bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
+        //setSupportActionBar(toolbar)
+
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment??:return
+        val navController = host.navController
+        setupBottomNavMenu(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,7 +40,12 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private fun setupBottomNavMenu(navController: NavController){
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    /*private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_timer -> {
                 toolbar.title = "Countdown"
@@ -49,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         }
         false
     }
-
+*/
 
 
     private fun openFragment(fragment: Fragment) {
